dec8f645491b748dd6d5c44b5a3be4d558d67ceb
stefan-icfo <stefan.forstner@icfo.eu>
2024-12-02 11:29:01 +0100

    hola

--- experiments/cs_linesweeps/chargesensing2d_qdac_zurich_fitv9.py
+++ experiments/cs_linesweeps/chargesensing2d_qdac_zurich_fitv9.py
@@ -60,13 +60,13 @@ zurich.oscs.oscs0.freq(mix_down_f)
 #outer gate voltage range (slow axis, 5gate)
 #####################
 
-idt_point1_x=-1.56488
-idt_point1_y=-1.55284
-idt_point2_x=-1.56047
-idt_point2_y=-1.54953
-delta=1000e-6
+idt_point1_x=-1.6747
+idt_point1_y=-1.645
+idt_point2_x=-1.67108
+idt_point2_y=-1.6407
+delta=1500e-6
 xi=0#move along ict (take traces not through centerbut closer to  triple pt)
-epsilon_0=0e-6#move prependicular to ict (compensate for drift)
+epsilon_0=-400e-6#move prependicular to ict (compensate for drift)
 start_vgo2,start_vgo1,stop_vgo2,stop_vgo1=make_detuning_axis_noncenterM(idt_point1_x,idt_point1_y,idt_point2_x,idt_point2_y,delta,xi,epsilon_0) 
 
 
@@ -75,7 +75,7 @@ start_vgo2,start_vgo1,stop_vgo2,stop_vgo1=make_detuning_axis_noncenterM(idt_poin
 
 
 postfix = f"xi={xi},epsilon_0={epsilon_0},g1={round(qdac.ch01.dc_constant_V(),2)},g3={round(qdac.ch03.dc_constant_V(),2)},g5={round(qdac.ch05.dc_constant_V(),2)}"
-step_vgo_num=60+1 #sqrt(100^2+200^2)uV
+step_vgo_num=50+1 #sqrt(100^2+200^2)uV
 
 
 
@@ -87,9 +87,9 @@ step_vgo2=np.absolute((start_vgo2-stop_vgo2)/step_vgo_num)
 
 #inner gate voltage range (fast axis, CS)
 #####################
-start_vgi = -2.235#-0.788
-stop_vgi = -2.232#-0.776
-step_vgi_num = 3*40#40uV
+start_vgi = -2.232#-0.788
+stop_vgi = -2.230#-0.776
+step_vgi_num = 2*40#40uV
 #step_vgi_num = round((stop_vgi-start_vgi)/vsd*upper_bound_lever_arm)
 #print(f"step i num={step_vgi_num}")
 step_vgi=np.absolute((start_vgi-stop_vgi)/step_vgi_num)

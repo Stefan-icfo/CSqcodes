47b94694e460b88e6cc672f8288c0e5c3377b7eb
stefan-icfo <stefan.forstner@icfo.eu>
2024-12-03 08:19:05 +0100

    hola

--- experiments/cs_mechanics/cs_mechanics_detune.py
+++ experiments/cs_mechanics/cs_mechanics_detune.py
@@ -55,9 +55,9 @@ idt_point1_x=-1.6747
 idt_point1_y=-1.645
 idt_point2_x=-1.67108
 idt_point2_y=-1.6407
-delta=1500e-6
+delta=2000e-6
 
-step_vgo_num =6+1 #
+step_vgo_num =10+1 #
 xi=0#move along ict (take traces not through centerbut closer to  triple pt)
 epsilon_0 =-400e-6#move prependicular to ict (compensate for drift)
 start_vgo2,start_vgo1,stop_vgo2,stop_vgo1=make_detuning_axis_noncenterM(idt_point1_x,idt_point1_y,idt_point2_x,idt_point2_y,delta,xi,epsilon_0) 
@@ -81,9 +81,9 @@ sitfraction=0.55#where to sit on Coulomb peak. For now on left side
 
 vars_to_save.extend([start_vgi,stop_vgi,step_vgi_num])
 #####################
-start_f = 401.45e6 #Hz unit
-stop_f =  402.75e6 #Hz unit
-step_num_f = 1300*3+1 #
+start_f = 401e6 #Hz unit
+stop_f =  407e6 #Hz unit
+step_num_f = 1000*6*5+1 #
 
 vars_to_save.extend([start_f,stop_f,step_num_f])
 
@@ -93,7 +93,7 @@ print(f"source amp at CNT for GVg:{source_amplitude_CNT_GVg*1e6} uV")
 source_amplitude_instrumentlevel_mech = 50e-3
 source_amplitude_CNT_mech=d2v(v2d(np.sqrt(1/2)*source_amplitude_instrumentlevel_mech)-att_source_dB)
 print(f"source amp at CNT for mech:{source_amplitude_CNT_mech*1e6} uV")
-gate_amplitude_instrumentlevel = 3.6e-3
+gate_amplitude_instrumentlevel =40e-3
 gate_amplitude_CNT=d2v(v2d(np.sqrt(1/2)*gate_amplitude_instrumentlevel)-att_gate_dB)
 print(f"gate amp at CNT for mech:{gate_amplitude_CNT*1e6} uV")
 

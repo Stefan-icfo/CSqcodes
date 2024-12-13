35ff57f7c0b995c7c9691c79f6ce921c33c6b38f
stefan-icfo <stefan.forstner@icfo.eu>
2024-12-13 12:03:45 +0100

    hola

--- experiments/DQD_charge_stability_viaCS_standard.py
+++ experiments/DQD_charge_stability_viaCS_standard.py
@@ -41,9 +41,9 @@ y_avg=-4.41e-6
 mix_down_f=1.25e6
 #outer voltage range (slow axis)
 #####################
-start_vg1 = -1.69#-1.934#
-stop_vg1 = -1.684#1.929 #delta 15
-step_vg1_num = 6*5#10uv
+start_vg1 = -1.68#-1.934#
+stop_vg1 = -1.665#1.929 #delta 15
+step_vg1_num = 15*5#10uv
 step_vg1=np.absolute((start_vg1-stop_vg1)/step_vg1_num)
 
 vars_to_save=[ramp_speed,step_ramp_speed,tc,att_source_dB,att_gate_dB,debug,x_avg,y_avg,mix_down_f,step_vg1]#more to add later
@@ -52,10 +52,10 @@ vars_to_save=[ramp_speed,step_ramp_speed,tc,att_source_dB,att_gate_dB,debug,x_av
 
 #inner voltage range (fast axis)
 #####################
-start_vg2 = -1.72#
-stop_vg2 =  -1.71#
+start_vg2 = -1.7#
+stop_vg2 =  -1.688#
 #stop_vg2 =  -1.571#-1.875#delta=10mV
-step_vg2_num=10*10
+step_vg2_num=12*10
 step_vg2=np.absolute((start_vg2-stop_vg2)/step_vg2_num)
 vars_to_save.append(step_vg2)
 
--- experiments/cs_linesweeps/chargesensing2d_qdac_zurich_fitv9.py
+++ experiments/cs_linesweeps/chargesensing2d_qdac_zurich_fitv9.py
@@ -60,13 +60,13 @@ zurich.oscs.oscs0.freq(mix_down_f)
 #outer gate voltage range (slow axis, 5gate)
 #####################
 
-idt_point1_x=-1.71669
-idt_point1_y=-1.6875
-idt_point2_x=-1.71378
-idt_point2_y=-1.6843
-delta=170e-6
+idt_point1_x=-1.69925
+idt_point1_y=-1.67068
+idt_point2_x=-1.6958
+idt_point2_y=-1.6667
+delta=450e-6
 xi=0#move along ict (take traces not through centerbut closer to  triple pt)
-epsilon_0=-100e-6#move prependicular to ict (compensate for drift)
+epsilon_0=0e-6#move prependicular to ict (compensate for drift)
 start_vgo2,start_vgo1,stop_vgo2,stop_vgo1=make_detuning_axis_noncenterM(idt_point1_x,idt_point1_y,idt_point2_x,idt_point2_y,delta,xi,epsilon_0) 
 
 
@@ -75,7 +75,7 @@ start_vgo2,start_vgo1,stop_vgo2,stop_vgo1=make_detuning_axis_noncenterM(idt_poin
 
 
 postfix = f"xi={xi},epsilon_0={epsilon_0},g1={round(qdac.ch01.dc_constant_V(),2)},g3={round(qdac.ch03.dc_constant_V(),2)},g5={round(qdac.ch05.dc_constant_V(),2)}"
-step_vgo_num=110+1 #sqrt(100^2+200^2)uV
+step_vgo_num=80+1 #sqrt(100^2+200^2)uV
 
 
 
